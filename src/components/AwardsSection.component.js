0ef4291f23727a4d06151b89a1b9acf11fe2823b
Saar Davidson <davidsonsaar@gmail.com>
2021-04-05 14:39:50 +0300

    update awards

--- src/components/AwardsSection.component.js
+++ src/components/AwardsSection.component.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import {
   FlexWrapper,
   BigHeadLine,
-  Text,
+  MediumHeadLine,
   SplitLogo,
 } from '../utils/constants/styledComponentsGlobal.constant';
 import split from '../assets/split.png';
@@ -16,29 +16,17 @@ function AwardsSection() {
     >
       <BigHeadLine color='white'> Awards </BigHeadLine>
       <AwardWrapper height='auto'>
-        <Text>
-          בית הספר למדעי המחשב בשיתוף חברת ספליט איט מזמינים אותכם/ן סטודנטים
-          לקחת חלק בהאקתון מאתגר בן עשרים ושש שעות בו תתעסקו יחד עם נציגי חברת
-          ספליט איט
-        </Text>
+        <MediumHeadLine>6,000₪</MediumHeadLine>
         <SplitLogo height='10rem' src={split} />
         <BigHeadLine color='turquoise'> 1 </BigHeadLine>
       </AwardWrapper>
       <AwardWrapper height='auto'>
-        <Text>
-          בית הספר למדעי המחשב בשיתוף חברת ספליט איט מזמינים אותכם/ן סטודנטים
-          לקחת חלק בהאקתון מאתגר בן עשרים ושש שעות בו תתעסקו יחד עם נציגי חברת
-          ספליט איט
-        </Text>
+        <MediumHeadLine>3,000₪</MediumHeadLine>
         <SplitLogo height='10rem' src={split} />
         <BigHeadLine color='turquoise'> 2 </BigHeadLine>
       </AwardWrapper>
       <AwardWrapper height='auto'>
-        <Text>
-          בית הספר למדעי המחשב בשיתוף חברת ספליט איט מזמינים אותכם/ן סטודנטים
-          לקחת חלק בהאקתון מאתגר בן עשרים ושש שעות בו תתעסקו יחד עם נציגי חברת
-          ספליט איט
-        </Text>
+        <MediumHeadLine>1,000₪</MediumHeadLine>
         <SplitLogo height='10rem' src={split} />
         <BigHeadLine color='turquoise'> 3 </BigHeadLine>
       </AwardWrapper>
